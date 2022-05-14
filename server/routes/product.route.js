96ba080deca097acc575e7bf874a36c6001f40f5
TenoriousOz <lucasprofessor.se@gmail.com>
2022-05-14 17:48:58 -0300

    falta fazer o postman funcionar

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,7 +1,18 @@
 const router = require("express").Router();
+const ProductModel = require("../models/Product.model");
 
-router.get('/hello', (req, res) => {
-  res.status(200).json({ msg: "Hello World!" });
+// router.get('/hello', (req, res) => {
+//   res.status(200).json({ msg: "Hello World!" });
+// });
+router.post("/product", async (req, res) => {
+  try {
+    const data = req.body;
+    const result = await ProductModel.create(data);
+    return res.status(201).json(result)
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({msg: "Falha ao cadastrar produto"});
+  }
 });
 
 module.exports = router;
