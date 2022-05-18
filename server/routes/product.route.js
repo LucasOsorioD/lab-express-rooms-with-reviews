67717b30198be8a6d6014c0b5388dc13e0ca2b63
TenoriousOz <lucasprofessor.se@gmail.com>
2022-05-17 22:47:55 -0300

    quase nada

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const ProductModel = require("../models/Product.model");
 
-// router.get('/hello', (req, res) => {
-//   res.status(200).json({ msg: "Hello World!" });
-// });
+router.get('/hello', (req, res) => {
+  res.status(200).json({ msg: "Hello World!" });
+});
 router.post("/product", async (req, res) => {
   try {
     const data = req.body;
